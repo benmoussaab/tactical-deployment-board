2c787ef1c46374623abeb8f6fa9181c80390e698
Benmoussaab <moussaab230@gmail.com>
2026-04-22 17:15:08 +0100

    Update kk.py

--- kk.py
+++ kk.py
@@ -71,7 +71,7 @@ CUSTOM_LOGOS = {
     "Tank1":         "https://i.imgur.com/TGHMPxV.png",
     "Helicopter":   "https://i.imgur.com/EcCleHZ.png",
     "Submarine":    "https://i.imgur.com/zXC7TnD.png",
-    "Drone": "https://imgur.com/a/rEucvWU.png",
+    "Drone": "https://imgur.com/a/drone-oPoyFNL.png",
     "Tank2": "https://imgur.com/a/pE2sS1B.png"
 }
 
